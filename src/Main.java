2589a44e43aa555531986ee45dfe561e0ad52cdf
Michael <oneillmichael@protonmail.com>
2021-09-01 10:22:49 +0100

    Project files included in push

--- src/Main.java
+++ src/Main.java
@@ -16,8 +16,8 @@ public class Main extends Application {
 
         stage.setTitle("LabManager for MULE");
         stage.setScene(scene);
-        //stage.setMinWidth(400);
-        //stage.setMinHeight(500);
+        //stage.setMinWidth(603);
+        //stage.setMinHeight(835);
         stage.getIcons().add(new Image("icon.png"));
         stage.show();
     }
